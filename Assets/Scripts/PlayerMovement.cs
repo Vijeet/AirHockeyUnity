9e1fe7c521b54f54bad6acc89491f347e5b01917
Vijeet Thomas <vijeetvthomas@gmail.com>
2023-12-24 12:07:40 +0530

    Adding assets

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    bool wasJustClicked = true;
+    bool canMove;
+    Vector2 playerSize;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSize = gameObject.GetComponent<SpriteRenderer>().bounds.extents; 
+        rb= GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (wasJustClicked)
+            {
+                if (
+                    (mousePos.x >= transform.position.x && mousePos.x < transform.position.x + playerSize.x 
+                    || mousePos.x <= transform.position.x && mousePos.x > transform.position.x - playerSize.x) 
+                    && 
+                    (mousePos.y >= transform.position.y && mousePos.y < transform.position.y + playerSize.y 
+                    || mousePos.y <= transform.position.y && mousePos.y > transform.position.y - playerSize.y)
+                    )
+                {
+                    canMove = true;
+                }
+                else
+                {
+                    canMove = false;
+                }
+            }
+            if (canMove)
+            {
+                rb.MovePosition(mousePos);
+            }
+        }
+        else
+        {
+            wasJustClicked = true;
+        }        
+    }
+}
